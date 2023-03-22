0598c5fea487ed317dda855b8638398a1c4b3f58
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-03-22 15:39:28 +0000

    the 'patern'ity testing, proto-pattern and the rule of three

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 
 // What is a Pattern ?
 // - a pattern is a reusable solution that can be applied to commonly problems in software design. (in our case, to apply it on javascript applications)
+
+// 'Pattern'-ity testing, proto-patterns & the rule of three
+//
+// what are proto-patterns?
+// in simple words, when someone creates a new pattern. it needs to be aproved from the community, so the pattern which doesn't pass the test yet, it calls proto-pattern
+//
+// what is patternity testing? it's the process of testing a proto-pattern, to see if it's a good one to use or not
+//
+// what is rule of three? when you want to create a pattern you must create at least three solutions for your problem and choose the bestb solution.
