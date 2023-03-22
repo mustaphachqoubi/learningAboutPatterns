216aaf0c065fa341091a7cea1e661138456ffc9f
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-03-22 12:59:42 +0000

    what is a patterns

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Let's get started
+
+// What is a Pattern ?
+// - a pattern is a reusable solution that can be applied to commonly problems in software design. (in our case, to apply it on javascript applications)
